2c1f12035f57b561b5b76791036caee4f0b00c1f
CaseDuncan <oluochdancun19@gmail.com>
2022-07-28 13:43:30 +0300

    defining paths

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom';
 
 function NavBar() {
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a href="" className="navbar-bran fw-bolder">E-store</a>
+        <NavLink href="" className="navbar-bran fw-bolder">E-store</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#NavbarCollapse">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="NavbarCollapse">
             <ul className="navbar-nav mx-auto">
                 <li className="nav-item">
-                    <a href="#" className="nav-link">Home</a>
+                    <NavLink to="/" className="nav-link">Home</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link">Products</a>
+                    <NavLink to="/products" className="nav-link">Products</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link">About</a>
+                    <NavLink to="/about" className="nav-link">About</NavLink>
                 </li>
                 <li className="nav-item">
-                    <a href="#" className="nav-link">Contact</a>
+                    <NavLink to="/contact" className="nav-link">Contact</NavLink>
                 </li>
 
             </ul>
