d97980c3938adab26a2bd8a00b12abf5d4ad1695
CaseDuncan <oluochdancun19@gmail.com>
2022-07-28 13:30:23 +0300

    handling routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import NavBar from "./Components/NavBar"
 import Home from "./Components/Home"
+import ProductsPage from './Components/ProductsPage';
+import {Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
-     <Home/>
+     <Routes>
+       <Route exact path="/" element={<Home/>}/>
+    
+       <Route exact path="/products" element={<ProductsPage/>}/>
+    
+     </Routes>
     </div>
   );
 }
