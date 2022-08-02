98916715850b9207f7f655aa0d78f490e5750fff
CaseDuncan <oluochdancun19@gmail.com>
2022-08-02 12:12:40 +0300

    reinstalling node modules

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
       <div className="card mb-3 mt-5 border-0">
       <div className="card bg-dark text-white">
-      <img src="../assets/image-3.jpg" className="card-img" style={{height : "550px"}} alt="..."/>
+      <img src="assets/image-3.jpg" className="card-img" style={{height : "550px"}} alt="..."/>
       <div className="card-img-overlay">
       <h5 className="card-title fw-bolder display-2 text-dark">NEW SEASONS ARRIVAL</h5>
       <p className="card-text lead fs-5 fw-bolder display-4">CHECK OUT ALL THE TRENDS.</p>
